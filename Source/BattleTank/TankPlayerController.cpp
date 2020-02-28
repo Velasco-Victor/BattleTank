56e4bbe04824b020196e4dbefebdf091b5095aa4
chiva2006 <v-gvictor@hotmail.com>
2020-02-27 21:27:01 -0800

    213. Using DeprojectScreenToWorld()

--- Source/BattleTank/TankPlayerController.cpp
+++ Source/BattleTank/TankPlayerController.cpp
@@ -38,13 +38,39 @@ void ATankPlayerController::AimTowardsCrosshair()
     if(GetSightRayHitLocation(HitLocation)) // Has Side effect, it is going to line trace
     {
         // TODO Tell controlled tank to aim at this point
-        UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString()); 
+        // UE_LOG(LogTemp, Warning, TEXT("Look Direction: %s"), *HitLocation.ToString()); 
     }
 }
     // Get world location of linetrace through crosshair, ture if hits landscape
 bool ATankPlayerController::GetSightRayHitLocation(FVector &OutHitLocation) const
 {
-    // If AimTowardsCrosshair hits landscape return true
-    OutHitLocation = FVector(1.0);
+    // Find the cross hair position
+    int32 ViewportSizeX, ViewportSizeY;
+    GetViewportSize(ViewportSizeX, ViewportSizeY);    
+    FVector2D ScreenLocation = FVector2D
+    (ViewportSizeX * CrossHairXLocation, ViewportSizeY *CrossHairYLocation); 
+
+    // "De-Project" the screen position of the crosshair to a wolrd direction
+    // Line trace along that look direction, and see what we hit (up to max range) 
+
+    FVector LookDirection;
+
+    //GetLookDirection(ScreenLocation, LookDirection);
+
+    if(GetLookDirection(ScreenLocation, LookDirection))
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Look Direction: %s"), *LookDirection.ToString());
+        // //Line trace
+        // GetLookVectorHitLocation(LookDirection, HitLocation);
+    }
+    
     return true;
+}
+
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const 
+{ FVector CameraWorldLocation; // To be discarded
+
+return DeprojectScreenPositionToWorld
+( ScreenLocation.X, ScreenLocation.Y, CameraWorldLocation, LookDirection ); 
+
 }
--- Source/BattleTank/TankPlayerController.h
+++ Source/BattleTank/TankPlayerController.h
@@ -15,19 +15,28 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
-	public:
+	private: 
 	ATank* GetControlledTank() const;
 
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void BeginPlay() override;
 
-
 	//Start the Tank moving the barrel so that a shot would hit where
 	//the crosshair intersects the world
 	void AimTowardsCrosshair();
 
+	FVector2D ScreenLocation();
+
 // Return an OUT parameter, true if hit landscape
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.333333f;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
 	
 };
